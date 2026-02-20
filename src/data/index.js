9f938843bd54b7c4e95a671bdb166934d5a67bbf
Xavier <xcalzada@gmail.com>
2026-02-20 01:13:11 +0100

    Mejorar colores, estetica y añadir compartivos

--- src/data/index.js
+++ src/data/index.js
@@ -1,14 +1,13 @@
 // 1. Importamos las unidades individuales
-import { houseUnit } from './units/house';
-import { recordsUnit } from './units/records';
-import { habitsPSUnit } from './units/habitsPS';
-import { comparativesUnit } from './units/comparatives';  // ← añade esta línea
- 
+//import { houseUnit } from './units/house';
+//import { recordsUnit } from './units/records';
+//import { habitsPSUnit } from './units/habitsPS';
+import { comparativesUnit } from './units/comparatives';   // ← importa la nueva unidad
 // 2. EXPORTAMOS el objeto con el nombre exacto que busca App.jsx
 export const UNITS_DATA = {
-  [houseUnit.id]: houseUnit,
-  [recordsUnit.id]: recordsUnit,
-  [habitsPSUnit.id]: habitsPSUnit,
+  //[houseUnit.id]: houseUnit,
+  //[recordsUnit.id]: recordsUnit,
+  //[habitsPSUnit.id]: habitsPSUnit,
   [comparativesUnit.id]: comparativesUnit,   // ← añade esta línea
 };
 
